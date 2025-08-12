bb17a500497eaa056917363daad3a33a82aa73e2
katsuta577 <koriyama550@gmail.com>
2025-08-12 11:58:02 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/repository_test_project/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.repository_test_project.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
